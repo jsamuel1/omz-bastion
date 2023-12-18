bd5ab4376c53601a77376fde945f1830a448e4dc
Joshua Samuel <sauhsoj@amazon.com>
2023-12-19 08:55:10 +1100

    now here

--- lib/omz-bastion-stack.ts
+++ lib/omz-bastion-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from 'aws-cdk-lib';
 import { aws_ec2 as ec2, aws_iam as iam } from 'aws-cdk-lib';
+import { UserData } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { config } from 'process';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -25,35 +26,45 @@ export class OmzBastionStack extends cdk.Stack {
       {
         configSets:
         {
-          default: ['yum', 'config', 'commands'],
+          default: ['config', 'ssh', 'yum', 'commands'],
         },
         configs:
         {
-          yum: new ec2.InitConfig([
-            ec2.InitPackage.yum('git'),
-          ]),
           config: new ec2.InitConfig([
-            ec2.InitGroup.fromName('docker'),
-            ec2.InitGroup.fromName('ssm-user', 1001),
-            ec2.InitUser.fromName('ssm-user',
-              {
-                homeDir: '/home/ssm-user',
-                userId: 1001,
-                groups: ['ssm-user', 'docker', 'wheel', 'adm'],
-              }),
+            ec2.InitCommand.shellCommand("while pgrep yum -c ; do sleep 5; done ", { ignoreErrors: true }),
+            ec2.InitCommand.shellCommand("yum update -y", { ignoreErrors: true }),
+            ec2.InitCommand.shellCommand("yum install -y dnf-plugins-core", { ignoreErrors: true }),
+            ec2.InitCommand.shellCommand("yum config-manager --add-repo https://rtx.pub/rpm/rtx.repo"),
+            ec2.InitCommand.shellCommand("yum config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo"),
+            ec2.InitCommand.shellCommand("yum config-manager --add-repo https://packages.microsoft.com/yumrepos/vscode"),
+            ec2.InitCommand.shellCommand("yum install -y git vim tmux zsh gh code", { ignoreErrors: true }),
+
+            // ec2.InitGroup.fromName('docker'),
+            //  ec2.InitGroup.fromName('ssm-user', 1001),
+            //  ec2.InitUser.fromName('ssm-user',
+            //    {
+            //      homeDir: '/home/ssm-user',
+            //      userId: 1001,
+            //      groups: ['ssm-user', 'docker', 'wheel', 'adm'],
+            //    }),
+            ec2.InitFile.fromString("/etc/sudoers.d/ssm-agent-users", "ssm-user ALL=(ALL) NOPASSWD:ALL",
+              { mode: "000440", }),
           ]),
-           commands: new ec2.InitConfig([
-          //   ec2.InitCommand.shellCommand(
-          //     `mkdir -p /home/ssm-user/src  \
-          //   && git clone ${gitUrl} /home/ssm-user/src/ \
-          //   && cd /home/ssm-user/src/${gitRepoProjectName} \
-          //   && ./bootstrap.sh \
-          //   `, { 
-          //     ignoreErrors: true,
-          //   }),
-            ec2.InitFile.fromFileInline('/home/ssm-user/.ssh/authorized_keys',
-               `${process.env.HOME}/.ssh/id_ecdsa.pub`,
-               { owner: "ssm-user", group: "ssm-user" }),
+          ssh: new ec2.InitConfig([
+            ec2.InitFile.fromFileInline('/etc/skel/.ssh/authorized_keys',
+              `${process.env.HOME}/.ssh/id_ed25519.pub`, { mode: "000644" }),
+          ]),
+          yum: new ec2.InitConfig(initPackagesYum()),
+
+          commands: new ec2.InitConfig([
+            ec2.InitCommand.shellCommand(`sed -i "s\\\\SHELL=.*\\\\SHELL=/usr/bin/zsh\\ \\" /etc/defaults/useradd`, { ignoreErrors: true }),
+            ec2.InitCommand.shellCommand(`
+            git clone ${gitUrl} /usr/src/${gitRepoProjectName} 
+            cd /usr/src/${gitRepoProjectName} || exit
+            HOME=/etc/skel ./bootstrap.sh 
+             `, {
+              ignoreErrors: true,
+            }),
           ]),
         },
       });
@@ -79,12 +90,29 @@ export class OmzBastionStack extends cdk.Stack {
       },
     });
 
+    host.role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore'));
+
     const instance = host.node.defaultChild as ec2.Instance;
     const cfnInstance = instance.node.defaultChild as ec2.CfnInstance;
     cfnInstance.keyName = 'id_ed25519';
 
-    new cdk.CfnOutput(this, 'BastionInstanceId', { value: instance.instanceId, exportName: 'BastionInstanceId'  });
-    new cdk.CfnOutput(this, 'BastionKeyName', { value: instanceName, exportName: 'BastionName'})
+    new cdk.CfnOutput(this, 'BastionInstanceId', { value: instance.instanceId, exportName: 'BastionInstanceId' });
+    new cdk.CfnOutput(this, 'BastionKeyName', { value: instanceName, exportName: 'BastionName' })
+    new cdk.CfnOutput(this, 'BootstrapCommand', { value: `
+    aws ssm start-session --target ${instance.instanceId} --document-name AWS-StartInteractiveCommand --parameters \ 
+    command="cd /usr/src/${gitRepoProjectName} && /usr/src/${gitRepoProjectName}/bootstrap.sh"
+    `, exportName: 'BootstrapCommand'})
   }
 }
 
+function initPackagesYum(): cdk.aws_ec2.InitElement[] {
+  // Generate an array of InitPackage with yum entries for each line in the file dnfrequirements.txt
+  const dnfRequirements = require('fs').readFileSync('dnfrequirements.txt', 'utf8').
+    split('\n').map((line: string) => line.trim()).flatMap((line: string) => {
+      if (line.length == 0) return [];
+      if (line.startsWith('#')) return [];
+      return [line];
+    });
+
+  return dnfRequirements.map((packageName: string) => ec2.InitPackage.yum(packageName));
+}
