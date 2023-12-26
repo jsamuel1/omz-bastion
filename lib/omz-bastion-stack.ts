a77da38c0b8a7497b3b17dd040b8a104fcaee65d
Joshua Samuel <sauhsoj@amazon.com>
2023-12-26 19:44:51 +1100

    tweak

--- lib/omz-bastion-stack.ts
+++ lib/omz-bastion-stack.ts
@@ -174,7 +174,7 @@ runcmd:
 
     cloudInitDynamicData.addCommands(
       '- stdbuf -oL nohup bash -c \\\\"$(curl -fsSL https://raw.githubusercontent.com/jsamuel1/dot-files/master/bootstrap.sh)\\\\" || echo "failed"',
-      '- stdbuf -oL nohup sudo -u ssm-user -Hin bash -c \\\\"$(curl -fsSL https://raw.githubusercontent.com/jsamuel1/dot-files/master/bootstrap.sh)\\\\" || echo "failed"',
+      '- stdbuf -oL nohup sudo -u ssm-user -Hin bash -c \'bash -c "$(curl -fsSL https://raw.githubusercontent.com/jsamuel1/dot-files/master/bootstrap.sh)" || echo "failed"\'',
       '',);
 
     cloudInitDynamicData.addCommands(
